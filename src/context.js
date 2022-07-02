1fe63259b950430b0ad1c530647b5d92a80c515b
Maksym Morenko <morenko.maxim@gmail.com>
2022-07-02 23:59:28 +0300

    Создание редьюсера, добавление первых кейсов

--- src/context.js
+++ src/context.js
@@ -1,5 +1,15 @@
 // 1. импортируем криейт кнтекст из реакта
-import { createContext } from "react";
+import { createContext, useReducer } from "react";
+import { reducer } from "./reducer";
+
+//эмулируем значения по умолчанию (берем из хуков сетстейт):
+const initialState = {
+    goods: [],
+    loading: true,
+    order: [],
+    isBasketShow: false,
+    alertName: '',
+};
 
 // создадим контекст магазина и запишем его в константу ShopContext
 export const ShopContext = createContext();
@@ -8,6 +18,14 @@ export const ShopContext = createContext();
 // создадим функцию контекс провайдер (обертку для передачи данных), 
 // которая будет принимать проп чилдрен и возвращать провайдер шопконтекста
 export const ContextProvider = ({children}) => {
+    const [value, dispatch] = useReducer(reducer, initialState); 
+    // юзРедюсер будет принимать нашу функцию редьюсер со свитчем и значение по умолчанию
+    // полученые ключи value будем спускать ниже
+    // dispatch функция обновления value
+    value.closeAlert = () => {
+        dispatch({type: 'CLOSE_ALERT'}); //dispatch будет принимать action {type, pyload}
+    };
+
     return <ShopContext.Provider>
         {children}
     </ShopContext.Provider>
